ceb6c2fc9a742af16fae8fce22e9455255ee554a
Prince Khatri <princekhatri1013@gmail.com>
2026-02-04 18:14:35 +0530

    Added project structure

---
+++ src/exception.py
@@ -0,0 +1,22 @@
+import sys
+import logger
+
+def error_message_details(error,error_details:sys):
+    _,_,exc_tb = error_details.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    line_no = exc_tb.tb_lineno
+    error_message = f"Exception Occured in File {file_name}\n\t\t At line no -> {line_no}\nMessage: {error}"
+
+    return error_message
+
+class CustomException(Exception):
+    def __init__(self,error_message,error_details:sys):
+        super().__init__(error_message)
+        self.error_message = error_message_details(error_message,error_details=error_details)
+    
+    def __str__(self):
+        return self.error_message
+
+
+
+
