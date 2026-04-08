f29334b9b3443ceac6ba72010acc6e52f4555f43
Prince Khatri <princekhatri1013@gmail.com>
2026-04-08 11:51:48 +0530

    fix: improve exception traceback

--- src/decorators.py
+++ src/decorators.py
@@ -1,7 +1,9 @@
+import functools
 from .exception import CustomException
 import sys
 
 def handle_exception(func):
+    @functools.wraps(func)
     def new_func(*args,**kwargs):
         try:
             return func(*args,**kwargs)
--- src/exception.py
+++ src/exception.py
@@ -1,7 +1,9 @@
 import sys
 
 def error_message_details(error,error_details:sys):
-    _,_,exc_tb = error_details.exc_info()
+    exc_tb = error.__traceback__
+    while exc_tb.tb_next:
+        exc_tb = exc_tb.tb_next
     file_name = exc_tb.tb_frame.f_code.co_filename
     line_no = exc_tb.tb_lineno
     error_message = f"Exception Occured in File {file_name}\n\t\t At line no -> {line_no}\nMessage: {error}"
